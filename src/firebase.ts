b9882a40f3f66ad6f34aeb60db4dbf3609012b9d
Himanshu2601-png <himanshusolanki2601@gmail.com>
2026-03-19 12:09:36 +0530

    fix: Correct Firebase storage bucket URL
    Updates the `storageBucket` URL in `firebase.ts` from `scholarstack-856b4.firebasestorage.app` to `scholarstack-856b4.appspot.com` to reflect the correct Firebase Storage domain.

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCRYKl-eFzharc_ooM58Jkfe7fuLPv4DWs",
   authDomain: "scholarstack-856b4.firebaseapp.com",
   projectId: "scholarstack-856b4",
-  storageBucket: "scholarstack-856b4.firebasestorage.app",
+  storageBucket: "scholarstack-856b4.appspot.com",
   messagingSenderId: "35137339817",
   appId: "1:35137339817:web:390b0c9d9fab2fb29e84b8"
 };
